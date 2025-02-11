c063ab90a657fd5639adf7d368ebd9d7034c4d8e
Ahmad <ahmadtm1372@gmail.com>
2025-02-11 12:48:41 +0330

    crispy forms are added.

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # third party apps
+    'crispy_forms',
+    "crispy_bootstrap5",
     # local
     'post',
+    'accounts',
+    
+
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+AUTH_USER_MODEL = 'accounts.CustomUser '
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,3 +135,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
